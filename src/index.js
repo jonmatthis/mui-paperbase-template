1660ffb1fdb7df9b5ab1b2e20bb229a8af1c6e0a
jonmatthis <jonmatthis@gmail.com>
2023-04-06 09:34:23 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Paperbase from "./Paperbase";
+
+function App() {
+  return <Paperbase />;
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
